d36adb40c05b52590e725244025ba3a2af04118e
hanjun <godwithme809@gmail.com>
2016-12-20 19:18:23 +0900

    Remove

--- projectbeta/deletedata.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once $_SERVER['DOCUMENT_ROOT'].'/preset.php';
-
-    $id = $_SESSION['login_user'];
- 	$sql = "DELETE FROM usercontent WHERE id='$id'";
-    $result = $mysqli->query($sql);
-        
-?>
--- projectbeta/getid.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/preset.php';
-
-echo $_SESSION["login_user"];
-
-?>
